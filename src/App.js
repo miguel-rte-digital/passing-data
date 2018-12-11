5168e40428fcf9eda32e5ef590e0f59cd375d042
miguel-rte-digital <miguel.castillo@rte.ie>
2018-12-11 16:12:21 +0000

    updatiung

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import ListProfiles from './listProfiles';
 import logo from './logo.svg';
 
 /*
@@ -107,6 +107,9 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <h2>Favorite Movies</h2>
+		<div> 
+
+		</div>
       </div>
     );
   }
